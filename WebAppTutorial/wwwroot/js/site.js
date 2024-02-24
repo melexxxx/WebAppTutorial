e4dd7a085641a7e349ba77e31e74c0cf6a1497f9
Daniel Mieleniewski <daniel.mieleniewski@gmail.com>
2024-02-24 13:15:08 +0000

    * .gitignore: VS first .nt app push
    * WebAppTutorial.sln:
    * Startup.cs:
    * Program.cs:
    * appsettings.json:
    * Index.cshtml:
    * site.js:
    * Error.cshtml:
    * favicon.ico:
    * Privacy.cshtml:
    * site.css:
    * Index.cshtml.cs:
    * Error.cshtml.cs:
    * WebAppTutorial.csproj:
    * Privacy.cshtml.cs:
    * _ViewStart.cshtml:
    * _ViewImports.cshtml:
    * _Layout.cshtml:
    * appsettings.Development.json:
    * LICENSE:
    * launchSettings.json:
    * LICENSE.txt:
    * jquery.js:
    * jquery.min.js:
    * jquery.min.map:
    * LICENSE.md:
    * bootstrap.js:
    * bootstrap.css:
    * _ValidationScriptsPartial.cshtml:
    * bootstrap.min.js:
    * bootstrap.js.map:
    * bootstrap.css.map:
    * bootstrap.min.css:
    * bootstrap-grid.css:
    * bootstrap.bundle.js:
    * bootstrap.min.js.map:
    * bootstrap-reboot.css:
    * bootstrap.min.css.map:
    * LICENSE.txt:
    * bootstrap.bundle.js.map:
    * bootstrap.bundle.min.js:
    * jquery.validate.js:
    * bootstrap-grid.css.map:
    * bootstrap-grid.min.css:
    * bootstrap-reboot.css.map:
    * bootstrap-reboot.min.css:
    * additional-methods.js:
    * bootstrap.bundle.min.js.map:
    * jquery.validate.min.js:
    * bootstrap-grid.min.css.map:
    * bootstrap-reboot.min.css.map:
    * additional-methods.min.js:
    * jquery.validate.unobtrusive.js:
    * jquery.validate.unobtrusive.min.js:

---
+++ WebAppTutorial/wwwroot/js/site.js
@@ -0,0 +1,22 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your Javascript code.
+
+
+
+// Simple fade in/out animation
+function fadeInOut() {
+    let box = document.getElementById("animation-box");
+    let opacity = 1;
+    let intervalID = setInterval(function () {
+        if (opacity < 0.1) {
+            opacity = opacity + 0.1;
+        } else {
+            opacity = opacity - 0.1;
+        }
+        box.style.opacity = opacity;
+    }, 100);
+}
+window.onload = fadeInOut;
+
